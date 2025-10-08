ae30a9e02a278c7600fd1bc18709c505143083ce
asraful <asrafulislamasf@gmail.com>
2025-10-09 03:13:49 +0600

    Error page Design Added

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import Navbar from '../Header/Navbar';
+import Footer from '../Footer/Footer';
+import eroorpng from '../../assets/error-404.png'
+import { Link } from 'react-router';
 
 
 const ErrorPage = () => {
   
     return (
         <div>
-            <h1>Error 404 page Not Found</h1>
+            <Navbar></Navbar>
+               <div className='max-w-800px mx-auto py-10'>
+                   <div className='flex justify-center items-center'>
+                      <img src={eroorpng} alt="" />
+                   </div>
+                   <div className='flex flex-col justify-center items-center'>
+                     <h1 className='text-[2.2rem] font-medium text-black'>Oops, Page Not found!</h1>
+                     <p>The page you are looking for is not available.</p>
+                     <button className='my-5 text-[1rem] text-white font-semibold px-3 py-1.5 rounded-[.5rem] bg-gradient-to-r from-[#632EE3] to-[#9F62F2]'><Link to='/'>Go Back!</Link></button>
+                   </div>
+               </div>
+            <Footer></Footer>
         </div>
     );
 };
