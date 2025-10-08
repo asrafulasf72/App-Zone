555cba4b5568d4291bbb71c99a3d005ea3764e67
asraful <asrafulislamasf@gmail.com>
2025-10-08 23:47:45 +0600

    Search functionality added

--- src/Pages/Apps/Apps.jsx
+++ src/Pages/Apps/Apps.jsx
@@ -1,11 +1,14 @@
 import { Search } from 'lucide-react';
-import React from 'react';
+import React, { useState } from 'react';
 import Allapp from './Allapp';
 import useApphook from '../../Hooks/useApphook';
 
 const Apps = () => {
-
     const {appData}=useApphook();
+    const [search, setSearch]=useState('')
+    const term = search.trim().toLocaleLowerCase();
+    const searchItem= term ? appData.filter(items=> items.title.toLocaleLowerCase().includes(term)): appData;
+    
     return (
         <div className=' bg-gray-100'>
             <div className=' max-w-[1300px] mx-auto'>
@@ -15,14 +18,14 @@ const Apps = () => {
                 </div>
 
                 <div className='flex justify-between items-center px-2'>
-                    <h2 className='text-[1.5rem] font-semibold'>({appData.length}) Apps Founds</h2>
+                    <h2 className='text-[1.5rem] font-semibold'>({searchItem.length}) Apps Founds</h2>
                     <label>
-                        <input className='border-2 border-gray-300 px-3 py-1 rounded-[.4rem]' type="search" name="" placeholder='Search App'/>
+                        <input defaultValue={search} onChange={(e)=> setSearch(e.target.value)} className='border-2 border-gray-300 px-3 py-1 rounded-[.4rem]' type="search" name="" placeholder='Search App'/>
                     </label>
                 </div>
                  <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-3'>
                     {
-                        appData.map(allApp=> <Allapp key={allApp.id} allApp={allApp}></Allapp>)
+                        searchItem.map(allApp=> <Allapp key={allApp.id} allApp={allApp}></Allapp>)
                     }
                  </div>
             </div>
